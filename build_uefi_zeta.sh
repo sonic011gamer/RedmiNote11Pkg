1cf2f6cea47eec2ad56592ac0218fc94d61659c9
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-10-30 15:36:05 +0200

    this should still work, right?

--- build_uefi_zeta.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-stuart_setup -c Platforms/SurfaceDuo2Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_update -c Platforms/SurfaceDuo2Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_build -c Platforms/SurfaceDuo2Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
